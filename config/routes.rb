17e3218b61ffb139a8001a30294751f69ec95c7b
Minghua Zhao <minghz42@gmail.com>
2017-10-21 19:03:11 -0400

    install devise on development

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :rooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "rooms#index"
 end
